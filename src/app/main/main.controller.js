8d28c4da5ff18ac7b304056191412aa958e9e080
stefano solinas <obsidianart@gmail.com>
2016-04-23 12:45:02 +0100

    checking correct solution

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,31 +2,34 @@ export class MainController {
   constructor ($timeout, webDevTec, toastr) {
     'ngInject';
 
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1461405922599;
-    this.toastr = toastr;
+    this.guess = ''
+    this.isCorrectSolution = ()=> this.guess === 'sabbia'
+    
+    // this.awesomeThings = [];
+    // this.classAnimation = '';
+    // this.creationDate = 1461405922599;
+    // this.toastr = toastr;
 
-    this.activate($timeout, webDevTec);
+    // this.activate($timeout, webDevTec);
   }
 
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
+  // activate($timeout, webDevTec) {
+  //   this.getWebDevTec(webDevTec);
+  //   $timeout(() => {
+  //     this.classAnimation = 'rubberBand';
+  //   }, 4000);
+  // }
 
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
+  // getWebDevTec(webDevTec) {
+  //   this.awesomeThings = webDevTec.getTec();
 
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
+  //   angular.forEach(this.awesomeThings, (awesomeThing) => {
+  //     awesomeThing.rank = Math.random();
+  //   });
+  // }
 
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
-  }
+  // showToastr() {
+  //   this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+  //   this.classAnimation = '';
+  // }
 }
