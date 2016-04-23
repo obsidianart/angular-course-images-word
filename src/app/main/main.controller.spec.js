8c67883f95edab7799de27f0c3aecbb18103eb77
stefano solinas <obsidianart@gmail.com>
2016-04-23 15:27:21 +0100

    Fix unit test

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,32 +1,35 @@
 describe('controllers', () => {
-  let vm;
+  let vm
 
-  beforeEach(angular.mock.module('angularCourseImagesWord'));
+  beforeEach(angular.mock.module('angularCourseImagesWord'))
 
-  beforeEach(inject(($controller, webDevTec, toastr) => {
-    spyOn(webDevTec, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-    spyOn(toastr, 'info').and.callThrough();
+  beforeEach(inject(($controller, $rootScope, toastr) => {
+    //spyOn(webDevTec, 'getTec').and.returnValue([{}, {}, {}, {}, {}])
+    spyOn(toastr, 'info').and.callThrough()
+    let scope = $rootScope.$new()
 
-    vm = $controller('MainController');
-  }));
+    vm = $controller('MainController',{
+      $scope: scope
+    })
+  }))
 
-  it('should have a timestamp creation date', () => {
-    expect(vm.creationDate).toEqual(jasmine.any(Number));
-  });
+  it('should start at level 1', () => {
+    expect(vm.currentLevel).toEqual(2)
+  })
 
-  it('should define animate class after delaying timeout', inject($timeout => {
-    $timeout.flush();
-    expect(vm.classAnimation).toEqual('rubberBand');
-  }));
+  // it('should define animate class after delaying timeout', inject($timeout => {
+  //   $timeout.flush()
+  //   expect(vm.classAnimation).toEqual('rubberBand')
+  // }))
 
-  it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
-    vm.showToastr();
-    expect(toastr.info).toHaveBeenCalled();
-    expect(vm.classAnimation).toEqual('');
-  }));
+  // it('should show a Toastr info and stop animation when invoke showToastr()', inject(toastr => {
+  //   vm.showToastr()
+  //   expect(toastr.info).toHaveBeenCalled()
+  //   expect(vm.classAnimation).toEqual('')
+  // }))
 
-  it('should define more than 5 awesome things', () => {
-    expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-    expect(vm.awesomeThings.length === 5).toBeTruthy();
-  });
-});
+  // it('should define more than 5 awesome things', () => {
+  //   expect(angular.isArray(vm.awesomeThings)).toBeTruthy()
+  //   expect(vm.awesomeThings.length === 5).toBeTruthy()
+  // })
+})
